f14a851ec3fcd346f35b77e3e2df70fe35daed6b
HUE_TrashMe <heuhaufen@huechan.com>
2023-07-30 17:01:22 +0200

    Install Redis

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
+// src/index.ts
+// Huechan /place/ Backend
+// (c) 2023 HUE_TrashMe
+
 import { WebSocketServer } from "ws";
+
+async function main() {
+	// Create WebSocket server
+	const wss = new WebSocketServer({port: 947});
+	wss.on("connection", (ws) => {
+		ws.send("lol");
+	});
+}
+
+main();
