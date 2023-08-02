e0ef947b4dc2d0491dd511627288a100edf27091
HUE_TrashMe <heuhaufen@huechan.com>
2023-08-02 23:21:47 +0200

    Implement poll request action

--- src/poll.ts
+++ src/poll.ts
@@ -4,6 +4,7 @@
 
 import { Colors, Coordinates } from "./types";
 import { Response } from "./wss";
+import { Database } from "./database";
 
 /**
  * Returns a {@link PollResponse} containung the color and timestamp of the given tile.
@@ -17,7 +18,16 @@ export async function action_poll(coordinates: Coordinates) {
 		timestamp: 0
 	}
 	
-	// Get color value from database
+	// Get color and timestamp values from database
+	const color = await Database.getTile(coordinates);
+	const timestamp = await Database.getDrawTimestamp(coordinates);
+
+	return {
+		success: true,
+		coordinates: coordinates,
+		color: color,
+		timestamp: timestamp
+	} as PollResponse;
 }
 
 interface PollResponse extends Response {
