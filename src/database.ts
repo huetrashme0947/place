4cd863ee537ed94b6d249e62808412363a6c2049
HUE_TrashMe <heuhaufen@huechan.com>
2023-08-02 15:05:34 +0200

    Add JSDoc annotations to database.ts

--- src/database.ts
+++ src/database.ts
@@ -5,12 +5,15 @@
 import { createClient } from "redis";
 
 import { logger } from "./logging";
-import { Coordinates } from "./types";
+import { Colors, Coordinates } from "./types";
 import { getCurrentCanvasSize } from "./configuration";
 
 namespace Database {
 	let client = createClient();
 
+	/**
+	 * Initializes the Redis client and connects it to the database.
+	 */
 	async function setupClient() {
 		logger.info("[database] Connecting to database...");
 
@@ -25,7 +28,12 @@ namespace Database {
 		logger.info("[database] Ready");
 	}
 
-	export async function setTile(coordinates: Coordinates, value: number) {
+	/**
+	 * Overwrites the tile at the given coordinates with the given color and updates the corresponding timestamp.
+	 * @param coordinates Coordinates of the tile to be written
+	 * @param color Color to write to the given tile
+	 */
+	export async function setTile(coordinates: Coordinates, color: Colors) {
 		// Check if client is ready to answer requests, if not wait
 		if (!client.isReady) {
 			await setupClient();
@@ -36,26 +44,36 @@ namespace Database {
 			"operation": "SET",
 			"encoding": "u4",
 			"offset": await calculateTileOffset(coordinates),
-			"value": value
+			"value": color
 		}]);
 
 		// Update timestamp
 		await client.set(`place_timestamp:${coordinates[0]}:${coordinates[1]}`, Date.now());
 	}
 
+	/**
+	 * Returns the color of the given tile.
+	 * @param coordinates Coordinates of tile
+	 */
 	export async function getTile(coordinates: Coordinates) {
 		// Check if client is ready to answer requests, if not wait
 		if (!client.isReady) {
 			await setupClient();
 		}
 
-		return await client.bitField("place", [{
+		const [color] = await client.bitField("place", [{
 			"operation": "GET",
 			"encoding": "u4",
 			"offset": await calculateTileOffset(coordinates)
 		}]);
+
+		return color ? color : Colors.White;
 	}
 
+	/**
+	 * Returns the timestamp of when the given tile was last updated.
+	 * @param coordinates Coordinates of tile
+	 */
 	export async function getDrawTimestamp(coordinates: Coordinates) {
 		// Check if client is ready to answer requests, if not wait
 		if (!client.isReady) {
@@ -66,9 +84,8 @@ namespace Database {
 	}
 
 	/**
-	 * Calculates the offset used to address the database bitfield
-	 * @param {Coordinates} coordinates The coordinates of the tile to be addressed
-	 * @returns {Promise<number>}
+	 * Calculates the offset used to address the database bitfield based on the coordinates of a tile.
+	 * @param coordinates Coordinates of tile
 	 */
 	async function calculateTileOffset(coordinates: Coordinates) {
 		const [canvasWidth] = await getCurrentCanvasSize();
